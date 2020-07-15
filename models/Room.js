aab16f117ed0ece5db46ef830a9f69b11524093e
lekynam2000 <KYNAM001@e.ntu.edu.sg>
2020-07-16 00:38:47 +0800

    feat: add User and Room DB scheme

---
+++ models/Room.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const RoomSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = Room = mongoose.model('rooms', RoomSchema);
