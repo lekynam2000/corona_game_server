0c69aa3de04abf3863ffe812f4e532d9cc88ef6c
Le Ky Nam <lekynam2000@gmail.com>
2020-07-16 18:12:06 +0800

    feat: roles and phases enum

---
+++ enum/phases.js
@@ -0,0 +1,11 @@
+module.exports = {
+    start: "start",
+    quarantine:"quarantine",
+    move:"move",
+    doctor_scan:"doctor_scan",
+    doctor_cure:"doctor_cure",
+    distribute_mask: "distribute_mask",
+    active_infect:"active_infect",
+    random_infect:"random_infect",
+    end: "end"
+}
---
+++ enum/roles.js
@@ -0,0 +1,7 @@
+module.exports = {
+    super_infected : "super_infected",
+    super_infected_hidden : "super_infected_hidden",
+    mask_distributor : "mask_distributor",
+    doctor : "doctor",
+    police : "police"
+}
