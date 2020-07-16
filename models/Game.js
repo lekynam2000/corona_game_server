20358e986746a9948aa8d26791a0293ba36ceaa8
lekynam2000 <KYNAM001@e.ntu.edu.sg>
2020-07-16 22:21:47 +0800

    feat: use enum in schema

--- models/Game.js
+++ models/Game.js
@@ -1,49 +1,51 @@
 const mongoose = require('mongoose');
-
+const roles = require('../enum/roles');
+const phases = require('../enum/phases');
 const GameSchema = new mongoose.Schema({
   admin: {
     type: mongoose.Schema.Types.ObjectId,
-    ref:'users',
+    ref: 'users',
     required: true,
   },
-  map:[[
-      {type:Number}
-  ]],
-  players:[{
-    arr_id:{
-        type:Number
-    },
-    name:{
-      type: String,
-      required:true,
-    },
-    role:{
-        type:String
-    },
-    infected:{
-        type:Boolean,
-        default:false
-    },
-    disabled:{
-        type:Boolean,
-        default:false
-    },
-    has_mask:{
-        type:Boolean,
-        default:false
-    },
-    place:{
-        type:Number,
-        default: -1
-    }
-
-  }],
-    turn:{
+  map: [[{ type: Number }]],
+  players: [
+    {
+      arr_id: {
         type: Number,
-        default: 0
-    },
-    phase:{
+      },
+      name: {
         type: String,
+        required: true,
+      },
+      role: {
+        type: String,
+        enum: Object.values(roles),
+      },
+      infected: {
+        type: Boolean,
+        default: false,
+      },
+      disabled: {
+        type: Boolean,
+        default: false,
+      },
+      has_mask: {
+        type: Boolean,
+        default: false,
+      },
+      place: {
+        type: Number,
+        default: -1,
+      },
     },
+  ],
+  turn: {
+    type: Number,
+    default: 0,
+  },
+  phase: {
+    type: String,
+    enum: Object.values(phases),
+  },
 });
 module.exports = Game = mongoose.model('games', GameSchema);
