a2ab2e7df94ca34139fc40ebdf3df1c68dd1795e
lekynam2000 <KYNAM001@e.ntu.edu.sg>
2020-07-18 12:11:38 +0800

    fix: add normal role

--- enum/roles.js
+++ enum/roles.js
@@ -1,7 +1,8 @@
 module.exports = {
-    super_infected : "super_infected",
-    super_infected_hidden : "super_infected_hidden",
-    mask_distributor : "mask_distributor",
-    doctor : "doctor",
-    police : "police"
-}
+  super_infected: 'super_infected',
+  super_infected_hidden: 'super_infected_hidden',
+  mask_distributor: 'mask_distributor',
+  doctor: 'doctor',
+  police: 'police',
+  normal: 'normal',
+};
