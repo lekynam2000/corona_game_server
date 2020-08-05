cd0a882532a90f9b53bcf59bf14629ad400ef628
lekynam2000 <KYNAM001@e.ntu.edu.sg>
2020-08-05 23:48:02 +0800

    ready for heroku deploy

--- server.js
+++ server.js
@@ -33,3 +33,11 @@ nsp.on('connection', (socket) => {
   roomFunc(socket);
 });
 http.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
